7458bc2e2b03eef11eb0d86945a34f9078ff9257
Chris Wiegman <cwiegman@ufl.edu>
2018-01-12 14:38:12 -0500

    Allow for variable data structures

--- includes/classes/class-gf-secure-storage-addon.php
+++ includes/classes/class-gf-secure-storage-addon.php
@@ -510,14 +510,26 @@ class GF_Secure_Storage_Addon extends \GFAddOn {
 				foreach ( $field['inputs'] as $input ) {
 
 					if ( isset( $this->_entries[ $lead['id'] ][ $input['id'] ] ) ) {
-						$value[ $input['id'] ] = $this->_entries[ $lead['id'] ][ $input['id'] ];
+
+						if ( is_array( $value ) ) {
+
+							$value[ $input['id'] ] = $this->_entries[ $lead['id'] ][ $input['id'] ];
+
+						} else {
+
+							$value = $this->_entries[ $lead['id'] ][ $input['id'] ];
+
+						}
 					}
 				}
 
-				foreach ( $value as $index => $item ) {
+				if ( is_array( $value ) ) {
 
-					if ( 'ufh-gf-secured' === $item ) {
-						$value[ $index ] = '';
+					foreach ( $value as $index => $item ) {
+
+						if ( 'ufh-gf-secured' === $item ) {
+							$value[ $index ] = '';
+						}
 					}
 				}
 			} else {
