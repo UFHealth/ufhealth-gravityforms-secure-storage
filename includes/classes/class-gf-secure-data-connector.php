27e5d439ddf86070e7b033d231dfc39813465174
Chris Wiegman <cwiegman@ufl.edu>
2018-01-16 10:13:31 -0500

    Add a class to abstract the data connector

---
+++ includes/classes/class-gf-secure-data-connector.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * GF Secure Data Connector
+ *
+ * Allows for abstraction to connect to the data layer of choice for secure storage.
+ *
+ * @package UFHealth\Gravity_Forms_Secure_Storage
+ *
+ * @since   1.0
+ *
+ * @author  Chris Wiegman <cwiegman@ufl.edu>
+ */
+
+namespace UFHealth\Gravity_Forms_Secure_Storage;
+
+/**
+ * Class GF_Secure_Data_Connector
+ */
+class GF_Secure_Data_Connector {
+
+}
